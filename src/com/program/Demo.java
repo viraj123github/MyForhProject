bfe98c425ecf5ee7ac2176aa88deeee6c8fc697b
viraj phalke <virajphalke20@gmail.com>
2024-05-30 10:04:10 +0530

    My Second Commit

--- src/com/program/Demo.java
+++ src/com/program/Demo.java
@@ -3,16 +3,13 @@ package com.program;
 public class Demo {
 
 	public static void main(String[] args) {
-		Demo d=new Demo();
+		Demo d = new Demo();
 		d.m1();
-		
-				
-		
 
 	}
-	public void m1()
-	{
-		System.out.println("this is m1 in demo");
+
+	public void m1() {
+		System.out.println("this is a m1 in demo");
 	}
 
 }
